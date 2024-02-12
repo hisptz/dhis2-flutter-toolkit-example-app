6f88efe927f08acabcf3cc75510dc6803111ceff
Gift Nnko <nnkogift@hotmail.com>
2024-02-12 10:53:18 +0300

    feat(models): Implementation
    - Added data models for tracked entity attribute and related data

--- lib/models/metadata/attributeValue.dart
+++ lib/models/metadata/attributeValue.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 
+import 'package:dhis2_flutter_toolkit/models/metadata/trackedEntityAttributes.dart';
 import 'package:objectbox/objectbox.dart';
 
 import 'attribute.dart';
@@ -7,6 +8,7 @@ import 'attribute.dart';
 class DHIS2AttributeValue {
   final attribute = ToOne<DHIS2Attribute>();
   final dataElement = ToOne<DataElement>();
+  final trackedEntityAttribute = ToOne<TrackedEntityAttribute>();
   dynamic value;
 
   DHIS2AttributeValue(this.value);
---
+++ lib/models/metadata/trackedEntityAttributes.dart
@@ -0,0 +1,44 @@
+import 'package:dhis2_flutter_toolkit/models/metadata/attributeValue.dart';
+import 'package:dhis2_flutter_toolkit/models/metadata/legendSet.dart';
+import 'package:dhis2_flutter_toolkit/models/metadata/metadataBase.dart';
+import 'package:dhis2_flutter_toolkit/models/metadata/optionSet.dart';
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class TrackedEntityAttribute implements DHIS2MetadataResource {
+  @override
+  DateTime created;
+
+  @override
+  DateTime lastUpdated;
+
+  @override
+  @Unique()
+  String uid;
+
+  String name;
+  String code;
+
+  String formName;
+  String shortName;
+  String description;
+  String aggregationType;
+  String valueType;
+  bool zeroIsSignificant;
+  final attributeValues = ToMany<DHIS2AttributeValue>();
+  final legendSets = ToMany<LegendSet>();
+  final optionSet = ToOne<DHIS2OptionSet>();
+
+  TrackedEntityAttribute(
+      this.created,
+      this.lastUpdated,
+      this.uid,
+      this.name,
+      this.code,
+      this.formName,
+      this.shortName,
+      this.description,
+      this.aggregationType,
+      this.valueType,
+      this.zeroIsSignificant);
+}
