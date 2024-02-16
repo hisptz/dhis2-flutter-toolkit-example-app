d9bc5675c50e57020da5d9a8e9b56f17dd171702
Davis799 <davistibz@gmail.com>
2024-02-16 14:29:25 +0300

    feat(models): implementation
    - Added data model for AttributeValues

---
+++ lib/models/data/attributeValue.dart
@@ -0,0 +1,18 @@
+import 'package:dhis2_flutter_toolkit/models/metadata/attribute.dart';
+import 'package:dhis2_flutter_toolkit/models/metadata/dataElement.dart';
+import 'package:dhis2_flutter_toolkit/models/metadata/trackedEntityAttributes.dart';
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class DHIS2AttributeValue {
+  int id = 0;
+
+  final attribute = ToOne<DHIS2Attribute>();
+  final dataElement = ToOne<DataElement>();
+  final trackedEntityAttribute = ToOne<TrackedEntityAttribute>();
+  dynamic value;
+
+  DHIS2AttributeValue({this.value});
+
+  DHIS2AttributeValue.fromMap(Map json) : value = json["value"];
+}
