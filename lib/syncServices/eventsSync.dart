96e81a37f529003acdea012fd58b26f334cc1418
Davis799 <davistibz@gmail.com>
2024-02-27 12:17:03 +0300

    fix(sync services): fixes
    - Updated the events sync services

--- lib/syncServices/eventsSync.dart
+++ lib/syncServices/eventsSync.dart
@@ -1,4 +1,6 @@
+import 'package:dhis2_flutter_toolkit/models/data/dataValue.dart';
 import 'package:dhis2_flutter_toolkit/models/data/event.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/dataValue.dart';
 
 import 'package:dhis2_flutter_toolkit/repositories/data/event.dart';
 
@@ -27,5 +29,39 @@ class D2EventSync extends BaseTrackerSyncService<D2Event> {
   @override
   D2Event mapper(Map<String, dynamic> json) {
     return D2Event.fromMap(json);
+  }
+
+  @override
+  Future syncPage(int page) async {
+    Map<String, dynamic>? data = await getData<Map<String, dynamic>>(page);
+    if (data == null) {
+      throw "Error getting data for page $page";
+    }
+
+    List<Map<String, dynamic>> entityData =
+        data[dataKey ?? resource].cast<Map<String, dynamic>>();
+
+    List<Map<String, dynamic>> dataValues = entityData
+        .map((data) =>
+            {"event": data["event"], "dataValues": data["dataValues"]})
+        .toList();
+
+    List<D2Event> entities = entityData.map(mapper).toList();
+
+    await box.putManyAsync(entities);
+
+    final values = dataValues
+        .cast<Map>()
+        .map((data) => data["dataValues"]
+            .cast<Map>()
+            .map<D2DataValue>((attributeMap) =>
+                D2DataValue.fromMap(attributeMap, data["event"]))
+            .toList()
+            .cast<D2DataValue>())
+        .toList();
+
+    await Future.wait(values.map((data) async {
+      await d2DataValueBox.putAndGetManyAsync(data);
+    }));
   }
 }
