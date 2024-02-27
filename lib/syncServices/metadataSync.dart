6480bb7ed507617844ef751433e080770a0e5314
Davis799 <davistibz@gmail.com>
2024-02-27 12:18:24 +0300

    fix(sync service): - updated the metadata sync functions

--- lib/syncServices/metadataSync.dart
+++ lib/syncServices/metadataSync.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
+import 'package:dhis2_flutter_toolkit/models/data/enrollment.dart';
+import 'package:dhis2_flutter_toolkit/models/data/event.dart';
+import 'package:dhis2_flutter_toolkit/models/data/trackedEntity.dart';
 import 'package:dhis2_flutter_toolkit/models/metadata/organisationUnit.dart';
 import 'package:dhis2_flutter_toolkit/models/metadata/user.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/enrollment.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/event.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/relationship.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/trackedEntity.dart';
 import 'package:dhis2_flutter_toolkit/repositories/metadata/orgUnit.dart';
 import 'package:dhis2_flutter_toolkit/repositories/metadata/program.dart';
@@ -12,6 +16,7 @@ import 'package:dhis2_flutter_toolkit/syncServices/enrollmentSync.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/eventsSync.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/orgUnitSync.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/programSync.dart';
+import 'package:dhis2_flutter_toolkit/syncServices/relationshipSync.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/syncStatus.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/systemInfo.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/trackedEntitySync.dart';
@@ -38,6 +43,7 @@ class MetadataSync {
     List events = d2EventBox.getAll();
     List enrollments = d2EnrollmentBox.getAll();
     List teis = trackedEntityBox.getAll();
+    List relationships = relationshipBox.getAll();
     return userSyncService.isSynced() &&
         systemInfoSync.isSynced() &&
         orgUnits.isNotEmpty &&
@@ -69,9 +75,10 @@ class MetadataSync {
     D2OrganisationUnit defaultOrg = D2OrgUnitRepository().getDefaultOrgUnit();
 
     for (final program in programs) {
-      D2EventSync eventsSync = D2EventSync(program, defaultOrg.uid);
-      eventsSync.sync();
-      await controller.addStream(eventsSync.stream);
+      TrackedEntitySync trackedEntitySync =
+          TrackedEntitySync(program, defaultOrg.uid);
+      trackedEntitySync.sync();
+      await controller.addStream(trackedEntitySync.stream);
     }
 
     for (final program in programs) {
@@ -82,11 +89,40 @@ class MetadataSync {
     }
 
     for (final program in programs) {
-      TrackedEntitySync trackedEntitySync =
-          TrackedEntitySync(program, defaultOrg.uid);
-      trackedEntitySync.sync();
-      await controller.addStream(trackedEntitySync.stream);
+      D2EventSync eventsSync = D2EventSync(program, defaultOrg.uid);
+      eventsSync.sync();
+      await controller.addStream(eventsSync.stream);
     }
+
+    // List<D2Event>? events = D2EventRepository().getAll();
+    // List<D2Enrollment>? enrollments = D2EnrollmentRepository().getAll();
+    // List<TrackedEntity>? teis = TrackedEntityRepository().getAll();
+
+    // List<String> eventUid = events?.map((e) => e.uid).toList() ?? [];
+
+    // List<String> teiUid = teis?.map((e) => e.uid).toList() ?? [];
+
+    // List<String> enrollmentUid = enrollments?.map((e) => e.uid).toList() ?? [];
+
+    // for (final event in eventUid) {
+    //   RelationshipSync relationshipSync = RelationshipSync("event", event);
+    //   relationshipSync.sync();
+    //   await controller.addStream(relationshipSync.stream);
+    // }
+
+    // for (final enrollment in enrollmentUid) {
+    //   RelationshipSync relationshipSync =
+    //       RelationshipSync("enrollment", enrollment);
+    //   relationshipSync.sync();
+    //   await controller.addStream(relationshipSync.stream);
+    // }
+
+    // for (final tei in teiUid) {
+    //   RelationshipSync relationshipSync =
+    //       RelationshipSync("trackedEntity", tei);
+    //   relationshipSync.sync();
+    //   await controller.addStream(relationshipSync.stream);
+    // }
 
     controller.add(
         SyncStatus(synced: 0, total: 0, status: Status.complete, label: "All"));
