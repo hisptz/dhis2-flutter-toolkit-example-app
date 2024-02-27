663438676f78e6eb5d27a0e68a16e765c2ea2f2a
Davis799 <davistibz@gmail.com>
2024-02-27 12:15:31 +0300

    fix(repository): fixes
    - updated the trackedEntityAttributeValue repository

--- lib/repositories/data/trackedEntityAttributeValue.dart
+++ lib/repositories/data/trackedEntityAttributeValue.dart
@@ -7,22 +7,33 @@ import '../../objectbox.g.dart';
 final d2TrackedEntityAttributeValueBox =
     db.store.box<D2TrackedEntityAttributeValue>();
 
-class TrackedEntityAttributeValueRepository
+class D2TrackedEntityAttributeValueRepository
     extends BaseRepository<D2TrackedEntityAttributeValue> {
-  TrackedEntityAttributeValueRepository()
+  D2TrackedEntityAttributeValueRepository()
       : super(d2TrackedEntityAttributeValueBox);
 
   @override
-  D2TrackedEntityAttributeValue? getByUid(String uid) {
+  D2TrackedEntityAttributeValue? getById(int id) {
     Query<D2TrackedEntityAttributeValue> query =
         d2TrackedEntityAttributeValueBox
-            .query(D2TrackedEntityAttributeValue_.uid.equals(uid))
+            .query(D2TrackedEntityAttributeValue_.trackedEntityAttribute
+                .equals(id))
             .build();
     return query.findFirst();
   }
 
+  D2TrackedEntityAttributeValueRepository byTrackedEntity(int id) {
+    queryConditions = D2TrackedEntityAttributeValue_.trackedEntity.equals(id);
+    return this;
+  }
+
+  @override
+  D2TrackedEntityAttributeValue? getByUid(String uid) {
+    return null;
+  }
+
   @override
   D2TrackedEntityAttributeValue mapper(Map<String, dynamic> json) {
-    return D2TrackedEntityAttributeValue.fromMap(json);
+    return D2TrackedEntityAttributeValue.fromMap(json, "");
   }
 }
