282833d1bea048d151007cdb20b975de90bbd695
Davis799 <davistibz@gmail.com>
2024-02-28 17:40:33 +0300

    fix(model): fixes
    - Improved the fromRelationship model

--- lib/models/data/fromRelationship.dart
+++ lib/models/data/fromRelationship.dart
@@ -3,6 +3,7 @@ import 'package:dhis2_flutter_toolkit/models/data/event.dart';
 import 'package:dhis2_flutter_toolkit/models/data/trackedEntity.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/enrollment.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/event.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/fromRelationship.dart';
 import 'package:dhis2_flutter_toolkit/repositories/data/trackedEntity.dart';
 import 'package:objectbox/objectbox.dart';
 
@@ -23,15 +24,18 @@ class FromRelationship {
   FromRelationship.fromMap(Map json, String type, String relationshipId)
       : uid = json[type] {
     if (type == "trackedEntity") {
+      id = FromRelationshipRepository().getIdByUid(json["trackedEntity"]) ?? 0;
       trackedEntity.target =
           TrackedEntityRepository().getByUid(json["trackedEntity"]);
     }
 
     if (type == "enrollment") {
+      id = FromRelationshipRepository().getIdByUid(json["enrollment"]) ?? 0;
       enrollment.target = D2EnrollmentRepository().getByUid(json["enrollment"]);
     }
 
     if (type == "event") {
+      id = FromRelationshipRepository().getIdByUid(json["event"]) ?? 0;
       event.target = D2EventRepository().getByUid(json["event"]);
     }
   }
