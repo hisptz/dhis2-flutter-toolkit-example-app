39c3a290ecee286a1530a032306f01b57ab37251
Davis799 <davistibz@gmail.com>
2024-02-28 13:05:59 +0300

    fix(model): fixes
    - Improved the toRelatiosnhip model to include linkage to other models

--- lib/models/data/toRelationship.dart
+++ lib/models/data/toRelationship.dart
@@ -1,22 +1,37 @@
 import 'package:dhis2_flutter_toolkit/models/data/enrollment.dart';
 import 'package:dhis2_flutter_toolkit/models/data/event.dart';
 import 'package:dhis2_flutter_toolkit/models/data/trackedEntity.dart';
-import 'package:dhis2_flutter_toolkit/objectbox.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/enrollment.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/event.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/trackedEntity.dart';
 import 'package:objectbox/objectbox.dart';
 
 import '../../objectbox.g.dart';
-
-final toRelationshipBox = db.store.box<ToRelationship>();
 
 @Entity()
 class ToRelationship {
   int id = 0;
 
-  final trackedEntityInstance = ToOne<TrackedEntity>();
+  String uid;
+  final trackedEntity = ToOne<TrackedEntity>();
   final enrollment = ToOne<D2Enrollment>();
   final event = ToOne<D2Event>();
 
-  ToRelationship();
+  ToRelationship({required this.uid});
 
-  ToRelationship.fromMap(Map json);
+  ToRelationship.fromMap(Map json, String type, String relationshipId)
+      : uid = json[type] {
+    if (type == "trackedEntity") {
+      trackedEntity.target =
+          TrackedEntityRepository().getByUid(json["trackedEntity"]);
+    }
+
+    if (type == "enrollment") {
+      enrollment.target = D2EnrollmentRepository().getByUid(json["enrollment"]);
+    }
+
+    if (type == "event") {
+      event.target = D2EventRepository().getByUid(json["event"]);
+    }
+  }
 }
