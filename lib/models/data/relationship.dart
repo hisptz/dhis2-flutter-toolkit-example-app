e0ec3568d0e7bc25354c31dc4cb5e79002af9628
Davis799 <davistibz@gmail.com>
2024-02-28 13:05:00 +0300

    fix(model): fixes
    - Improved the relationship model to include links to other models

--- lib/models/data/relationship.dart
+++ lib/models/data/relationship.dart
@@ -1,7 +1,14 @@
 import 'package:dhis2_flutter_toolkit/models/data/dataBase.dart';
+import 'package:dhis2_flutter_toolkit/models/data/enrollment.dart';
+import 'package:dhis2_flutter_toolkit/models/data/event.dart';
 import 'package:dhis2_flutter_toolkit/models/data/fromRelationship.dart';
 import 'package:dhis2_flutter_toolkit/models/data/toRelationship.dart';
-
+import 'package:dhis2_flutter_toolkit/models/data/trackedEntity.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/enrollment.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/event.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/fromRelationship.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/toRelationship.dart';
+import 'package:dhis2_flutter_toolkit/repositories/data/trackedEntity.dart';
 import 'package:objectbox/objectbox.dart';
 
 import '../../objectbox.g.dart';
@@ -22,8 +29,11 @@ class Relationship extends D2DataResource {
   bool bidirectional;
   String relationshipType;
 
-  final from = ToOne<FromRelationship>();
   final to = ToOne<ToRelationship>();
+  final from = ToOne<FromRelationship>();
+  final trackedEntity = ToOne<TrackedEntity>();
+  final enrollment = ToOne<D2Enrollment>();
+  final event = ToOne<D2Event>();
 
   Relationship({
     required this.createdAt,
@@ -34,18 +44,29 @@ class Relationship extends D2DataResource {
     required this.bidirectional,
   });
 
-  Relationship.fromMap(Map json)
+  Relationship.fromMap(Map json, String type)
       : createdAt = DateTime.parse(json["createdAt"]),
         updatedAt = DateTime.parse(json["updatedAt"]),
         uid = json["relationship"],
         relationshipName = json["relationshipName"],
         relationshipType = json["relationshipType"],
         bidirectional = json["bidirectional"] {
-    FromRelationship from = FromRelationship.fromMap(json["from"]);
-    ToRelationship to = ToRelationship.fromMap(json["to"]);
-    fromRelationshipBox.putAndGetAsync(from);
-    toRelationshipBox.putAndGetAsync(to);
+    if (type == "trackedEntity") {
+      trackedEntity.target =
+          TrackedEntityRepository().getByUid(json["from"][type][type]);
+    }
 
-    // from.target = FromRelationshipRepository().getByUid(json["from"]);
+    if (type == "enrollment") {
+      enrollment.target =
+          D2EnrollmentRepository().getByUid(json["from"][type][type]);
+    }
+
+    if (type == "event") {
+      event.target = D2EventRepository().getByUid(json["from"][type][type]);
+    }
+
+    from.target =
+        FromRelationshipRepository().getByUid(json["from"][type][type]);
+    to.target = ToRelationshipRepository().getByUid(json["to"][type][type]);
   }
 }
