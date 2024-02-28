4dd196030d9e77650fbc9d2dfd7e62ddda291d6d
Davis799 <davistibz@gmail.com>
2024-02-28 17:49:08 +0300

    refactor(sync): refactoring
    - metadata sync

--- lib/syncServices/metadataSync.dart
+++ lib/syncServices/metadataSync.dart
@@ -20,10 +20,8 @@ import 'package:dhis2_flutter_toolkit/syncServices/syncStatus.dart';
 import 'package:dhis2_flutter_toolkit/syncServices/systemInfo.dart';
 
 import 'package:dhis2_flutter_toolkit/syncServices/trackedEntitySync.dart';
-import 'package:dhis2_flutter_toolkit/syncServices/userRepository.dart';
 
 import 'package:dhis2_flutter_toolkit/syncServices/userSync.dart';
-
 
 class MetadataSync {
   UserSyncService userSyncService = UserSyncService();
